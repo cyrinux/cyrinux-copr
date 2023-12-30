51335c5cb1cfaa41f04770507e7123bf80245539
Cyril LEVIS <git@levis.name>
2023-12-30 10:08:32 +0100

    chore: test

--- rust-dfrs/create.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-crate="$(basename $(pwd))"
-echo "Creating crate $crate"
-copr add-package-custom misc --script ./bootstrap --name $crate --script-builddeps "curl rust2rpm git" --webhook-rebuild on
-copr build-package --nowait misc --name $crate
